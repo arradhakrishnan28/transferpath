cdfa775655348302ae5bd89caa068a583558125a
Aditya  Rajiv <adityarajiv@Adityas-MacBook-Air-3.local>
2026-06-21 02:29:52 -0500

    Initial Spring Boot setup

---
+++ src/main/java/com/transferpath/transferpath/TransferpathApplication.java
@@ -0,0 +1,13 @@
+package com.transferpath.transferpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferpathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferpathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transferpath/transferpath/TransferpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transferpath.transferpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferpathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
